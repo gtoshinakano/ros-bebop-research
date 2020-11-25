c0c2b875c62355b7007d80bf275406f30737e559
Gabriel Toshinori Nakano <gtoshinakano@gmail.com>
2020-11-25 20:37:20 +0900

    Handling api get request error

--- pages/api/home.js
+++ pages/api/home.js
@@ -1,15 +1,19 @@
 import matter from 'gray-matter'
 
 export default async (req, res) => {
-  console.log(req.query.language)
-  const common = await import('@Contents/home/common.md')
-  const meta = matter(common.default)
-  const content = await import('@Contents/home/'+req.query.language+'.md')
-  const parsedContent = matter(content.default)
-  const seo = {...meta.data, ...parsedContent.data}
-  res.statusCode = 200
-  res.json({ 
-    seo: seo,
-    markdownBody: parsedContent.content,
-  })
+  if(!req.query.language){
+    res.statusCode = 500
+    res.send("Error: Something went wrong")
+  } else{
+    const common = await import('@Contents/home/common.md')
+    const meta = matter(common.default)
+    const content = await import('@Contents/home/'+req.query.language+'.md')
+    const parsedContent = matter(content.default)
+    const seo = {...meta.data, ...parsedContent.data}
+    res.statusCode = 200
+    res.json({ 
+      seo: seo,
+      markdownBody: parsedContent.content,
+    })
+  }
 }
