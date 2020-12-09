6caab18087d8801a49957de1964966fcfb4a94dd
Gabriel Toshinori Nakano <gtoshinakano@gmail.com>
2020-12-09 11:14:11 +0900

    Added avatar to header section

--- src/Components/DefaultHero/index.js
+++ src/Components/DefaultHero/index.js
@@ -2,8 +2,12 @@ import {useRef, useEffect} from "react"
 import gsap from "gsap"
 import styled from "styled-components"
 import {breakpoint} from '@Definitions/Styled'
+import { withTranslation } from "../../../i18n"
+import Link from 'next/link'
 
-const DefaultHero = (props) => {
+const Hero = (props) => {
+
+  const {t} = props
 
   return(
     <HeroContainer
@@ -11,6 +15,14 @@ const DefaultHero = (props) => {
     >
       <Container>
         <h1>{props.header}</h1>
+        <Avatar>
+          <img src="https://scontent-lax3-2.xx.fbcdn.net/v/t1.0-9/18951501_10203096811459409_400331603628819510_n.jpg?_nc_cat=111&ccb=2&_nc_sid=09cbfe&_nc_ohc=Jw9qpGIiSDkAX83LVoT&_nc_ht=scontent-lax3-2.xx&oh=70bc074e1fcd5c6e6bff3b8185858e36&oe=5FF77F16" alt />
+          <small>
+            <b>{t("author")}</b>
+            <br /> <span>Dezembro</span>
+          </small>
+          
+        </Avatar>
       </Container>   
     </HeroContainer>
   )
@@ -22,17 +34,20 @@ const HeroContainer = styled.div`
   background-size: cover;
   width: 100%;
   min-height: 190px;
-  padding:20px 0;
+  padding:35px 0;
   display:flex;
+  font-family: Noto Sans JP; 
+  text-shadow: 0 0 0.5em ${props => props.theme.palette.common.contrastBg};
+  color: ${props => props.theme.palette.common.contrastText};
   h1{
     padding:0;
-    text-shadow: 0 0 0.5em ${props => props.theme.palette.common.contrastBg};
-    color: ${props => props.theme.palette.common.contrastText};
+    font-weight:600;
   }
 `
 
 const Container = styled.div`
-  max-width:680px;
+  max-width:880px;
+  width:100%;
   margin: 0 auto;
   ${breakpoint.xs} {
     margin:0 10px;
@@ -42,5 +57,20 @@ const Container = styled.div`
   }
 `
 
+const Avatar = styled.div`
+  margin-top:32px;
+  display:flex;
+  height:32px;
+  img{
+    border-radius:50%;
+    max-width:32px;
+  }
+  small{
+    margin:0 0 0 10px;
+    line-height: 1.3em;
+  }
+  span{font-weight:100;} 
+`
 
-export {DefaultHero}
+const Extended = withTranslation('common')(Hero)
+export {Extended as DefaultHero}
