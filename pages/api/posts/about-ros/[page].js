5c41b3888467e1516567bc2b726c3a1c597e0662
Gabriel Toshinori Nakano <gtoshinakano@gmail.com>
2020-12-25 16:20:16 +0900

    Prepared file structure for about-ros contents

---
+++ pages/api/posts/about-ros/[page].js
@@ -0,0 +1,24 @@
+import matter from 'gray-matter'
+import _ from 'lodash'
+
+const langs = ["en", "pt", "jp"]
+
+export default async (req, res) => {
+  const page = req.query.page // beginner-commands with-python
+
+  if(!req.query.language || !_.includes(langs, req.query.language)){
+    res.statusCode = 404
+    res.send("Error: Something went wrong")
+  } else {//TODO Logic to handle dynamic page import 
+    let common = await import(`@Contents/about-ros/beginner-commands/common.md`)
+    let content = await import(`@Contents/about-ros/beginner-commands/${req.query.language}.md`)
+    const meta = matter(common.default)
+    const parsedContent = matter(content.default)
+    const seo = {...meta.data, ...parsedContent.data}
+    res.statusCode = 200
+    res.json({ 
+      seo: seo,
+      markdownBody: parsedContent.content,
+    })
+  }
+}
