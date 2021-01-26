8dcc9d64fa4f6081a8440eda4c96d32af6701851
Gabriel Toshinori Nakano <gtoshinakano@gmail.com>
2021-01-26 22:16:30 +0900

    Changed to facebook photo

--- pages/api/profile.js
+++ pages/api/profile.js
@@ -1,7 +1,7 @@
 const urlMetadata = require('url-metadata')
 
 export default async (req, res) => {
-  const metadata = await urlMetadata('https://github.com/gtoshinakano')
+  const metadata = await urlMetadata('https://www.facebook.com/toshi.nakano.9')
   res.statusCode = 200
   res.json({ image: metadata.image })
 }
